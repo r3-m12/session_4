ada59e8e9fc44b5d5050e57b1b4a0fec715b4e75
Rem <6224734@cgodin.qc.ca>
2025-02-06 12:01:25 -0500

    variable1_variable2

---
+++ pyton.py
@@ -0,0 +1,13 @@
+a = 33 #variable 1 
+b = 200 #variable 2 
+c = "bonjours" #variable 3 
+is_on = False
+i = 0 
+while i < 6:
+  i += 1
+  if is_on:
+    print("Allo")
+  else:
+    print("Bonjours")
+  is_on = not is_on
+print(c)
